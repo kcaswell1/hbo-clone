abd3d7c1e959ce63220b1c330a81890e5a9a4658
kcaswell1 <50675382+kcaswell1@users.noreply.github.com>
2022-07-29 10:00:29 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,9 @@ export default function Document() {
     <Html>
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com"/>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;700&display=swap" rel="stylesheet"/>
-<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"></script>
 
         </Head>
       <body>
